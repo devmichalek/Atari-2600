24d39e6aebe611f85ab96799291f64cd88029d95
Adrian Michałek <devmichalek@gmail.com>
2020-10-27 19:16:45 +0100

    Addressing modes explanation

---
+++ AddressingModes/addressing.asm
@@ -0,0 +1,3 @@
+lda #80 ; Loads the A register with the literal decimal value 80
+lda $80 ; Loads the A register with the value inside memory address $80
+lda #$80 ; Loads the A register with the literal hexadecimal number $80
